ea576ade62ac865fbd5226468c197c497ec4d9ba
jhon <jhongonzales.contabilidad13@gmail.com>
2024-04-19 13:42:18 -0500

    aplicacion del clima

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import Image1 from './assets/background/background1.jpg';
+import Image2 from './assets/background/background2.jpg';
+import Image3 from './assets/background/background3.jpg';
+import Image4 from './assets/background/background4.jpg';
+import Image5 from './assets/background/background5.jpg';
+import Image6 from './assets/background/background6.jpg';
+import Image7 from './assets/background/background7.jpg';
+import Image8 from './assets/background/background8.jpg';
+import WeatherCard from './components/WeatherCard';
+import CurrentDate from './components/CurrentDate';
+import ScreenLoad from './components/ScreenLoad';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+  const [coords, setCoords] = useState()
+  const [weather, setWeather] = useState()
+  const [temp, setTemp] = useState()
+
+  const changeBackground = () => {
+    if (weather?.weather[0].icon === '50n') {
+      Image = Image1
+    }
+    else if (weather?.weather[0].icon === '09n' || '10n' || '11n') {
+      Image = Image2
+    }
+    else if (weather?.weather[0].icon === '01n') {
+      Image = Image3
+    }
+    else if (weather?.weather[0].icon === '02n' || '03n' || '04n') {
+      Image = Image4
+    }
+    else if (weather?.weather[0].icon === '50d') {
+      Image = Image5
+    }
+    else if (weather?.weather[0].icon === '01d') {
+      Image = Image6
+    }
+    else if (weather?.weather[0].icon === '02d' || '03d' || '04d') {
+      Image = Image7
+    }
+    else if (weather?.weather[0].icon === '09d' || '10d' || '11d') {
+      Image = Image8
+    }
+    return Image
+  };
+  changeBackground();
+
+  useEffect(() => {
+    const success = pos => {
+      const obj = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude
+      }
+      setCoords(obj)
+    }
+    navigator.geolocation.getCurrentPosition(success)
+  }, [])
+
+  useEffect(() => {
+    if(coords){
+      const apiKey = '2e2288d7dc7b53e0230ccb192b003ab4'
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}`
+      axios.get(url)
+      .then(res => {
+        setWeather(res.data)
+        const obj = {
+          celsius: (res.data.main.temp - 273.15).toFixed(1),
+          farenheit: ((res.data.main.temp - 273.15) * 9/5 + 32).toFixed(1)
+          }
+          setTemp(obj)
+        })
+      .catch(err => console.log(err))
+    }
+  }, [coords])
+
+  return (
+    <article className='hero display-flex' style={{ backgroundImage: `url(${Image})`}}>
+      <div className='article-container'>
+        <div>
+          <ScreenLoad/>
+        </div>
+        <div>
+          <WeatherCard
+            weather={weather}
+            temp={temp}/>
+        </div>
+        <div>
+          <CurrentDate/>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default App
